15e9e183d8acce331d8f2321a8247fc8764ada19
unknown <yuriy.kuzin.du@gmail.com>
2020-04-18 13:39:30 +0300

    added model and routes

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Service;
+use Faker\Generator as Faker;
+
+$factory->define(Service::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
