ee997b55e461a99524224200b52b5f6e7170e400
unknown <yuriy.kuzin.du@gmail.com>
2020-04-19 19:03:45 +0300

    Exceptions handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +53,19 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if($request->expectsJson())
+        {
+            if ($exception instanceof ModelNotFoundException)
+                return response()->json([
+                    'errors'    =>  'Company Model Not Found'
+                ], Response::HTTP_NOT_FOUND);
+            if ($exception instanceof NotFoundHttpException)
+                return response()->json([
+                    'errors'    =>  'Inccorect route'
+                ], Response::HTTP_NOT_FOUND);
+        }
+        
+
         return parent::render($request, $exception);
     }
 }
